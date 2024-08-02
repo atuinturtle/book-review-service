b2a97e8d73f85f60476ad939e37055f57ce83a53
mmakay <mmakay@iakos.com>
2024-08-02 11:33:44 +0200

    Initial commit.

---
+++ src/main/java/com/mmakay/bookreviewservice/BookReviewServiceApplication.java
@@ -0,0 +1,13 @@
+package com.mmakay.bookreviewservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookReviewServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookReviewServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mmakay/bookreviewservice/BookReviewServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mmakay.bookreviewservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookReviewServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
